6d5ecd1eda42097e77b66a75769d1b14ab79df26
Sayan <sayan.das@idevlive.com>
2026-05-29 21:23:50 +0530

    Added Statcounter tracking code to the landing page

--- resources/views/templates/landing.blade.php
+++ resources/views/templates/landing.blade.php
@@ -126,5 +126,22 @@
 	</script>
 
 	@yield("script")
+
+	<!-- Default Statcounter code for Quickbook Proadvisors https://quickbookproadvisors.us/ -->
+	<script type="text/javascript">
+	var sc_project=12948122; 
+	var sc_invisible=1; 
+	var sc_security="ea64a23c"; 
+	</script>
+	<script type="text/javascript"
+	src="https://www.statcounter.com/counter/counter.js"
+	async></script>
+	<noscript><div class="statcounter"><a title="Web Analytics
+	Made Easy - Statcounter" href="https://statcounter.com/"
+	target="_blank"><img class="statcounter"
+	src="https://c.statcounter.com/12948122/0/ea64a23c/1/"
+	alt="Web Analytics Made Easy - Statcounter"
+	referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+	<!-- End of Statcounter Code -->
 </body>
 </html>
