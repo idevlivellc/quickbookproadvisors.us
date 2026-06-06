a1de0d7d311b30b715a6aec88014ca44028480b1
Sayan <sayan.das@idevlive.com>
2026-06-06 19:27:20 +0530

    Added Clarity tracking script and gtag to the landing page.

--- resources/views/templates/landing.blade.php
+++ resources/views/templates/landing.blade.php
@@ -10,6 +10,24 @@
 	<link rel="icon" type="image/x-icon" href="{{ url("images/favicon.ico") }}">
 
 	@vite(["resources/css/website.css"])
+
+	<script type="text/javascript">
+		(function(c,l,a,r,i,t,y){
+			c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+			t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+			y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+		})(window, document, "clarity", "script", "x2ti6hu3se");
+	</script>
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18160661655"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'AW-18160661655');
+	</script>
 </head>
 <body>
 	<nav class="website">
